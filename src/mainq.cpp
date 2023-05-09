11e694c98c7d7f5f6ba73b77d9bd38997ee1aceb
Quentin Jungo <quentin.jungo@gmail.com>
2023-05-09 11:41:34 +0200

    add test main

--- src/mainq.cpp
+++ src/mainq.cpp
@@ -1,17 +1,19 @@
-//
-// Created by qjungo on 5/9/23.
-//
+#include "server//HttpRequest.h"
+#include "config/Config.h"
+#include "cgi_executor/CgiExecutor.h"
+#include <iostream>
+
 int main()
 {
-std::vector<Config *> servers = Config::parse_servers("/home/qjungo/Cursus/webserv/config/default.yaml");
+	std::vector<Config *> servers = Config::parse_servers("/home/qjungo/Cursus/webserv/config/default.yaml");
 //    servers[0]->log();
 //	HttpRequest request("GET /linux_cgi HTTP/1.1\r\nUser-Agent: curl/7.68.0\r\nAccept: */*\r\n\r\n");
-HttpRequest request("GET /new_user HTTP/1.1\r\nUser-Agent: curl/7.68.0\r\nAccept: */*\r\n\r\n");
-CgiExecutor cgi_executor;
-request.log();
+	HttpRequest request("GET /new_user HTTP/1.1\r\nUser-Agent: curl/7.68.0\r\nAccept: */*\r\n\r\n");
+	CgiExecutor cgi_executor;
+	request.log();
 
-std::cout << "======================" << std::endl;
-std::string res = cgi_executor.execute(request, *servers[0]);
-std::cout << "======================" << std::endl;
-std::cout << res << std::endl;
+	std::cout << "======================" << std::endl;
+	std::string res = cgi_executor.execute(request, *servers[0]);
+	std::cout << "======================" << std::endl;
+	std::cout << res << std::endl;
 }
