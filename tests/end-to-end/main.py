a44532a0761b4fe2dd66e90f7da0ad495b0c62a5
Quentin Jungo <quentin.jungo@gmail.com>
2023-05-13 09:23:24 +0200

    mastering the art of color ascii printing

--- tests/end-to-end/main.py
+++ tests/end-to-end/main.py
@@ -1,26 +1,36 @@
 import subprocess
-import time
 
 import requests
 
 GREEN = "\033[92m"
 RED = "\033[91m"
+BLUE = "\033[94m"
 END = "\033[0m"
+
+ERROR_404 = "<!DOCTYPE html><html><head><title>Error Page</title></head><body>" \
+            "<h1 style=\"text-align:center;\">404 Not Found</h1><p style=\"text-align:center;\">" \
+            "42_webserv</p></body></html>"
+
+HOST = "http://localhost:8080"
 
 
 def color_print(text, color):
     print(color + text + END)
 
 
-def error_print(text):
-    print(RED + "ERROR: \n\t- " + text + END)
+def error_print(text, url=None):
+    print(RED + "ERROR: \n\t- " + text, end=" ")
+    if url:
+        print("for URL: " + url, end="")
+    print(END)
 
 
 def assert_get(url, expected_status_code, expected_response):
     try:
         response = requests.get(url)
         if response.status_code != expected_status_code:
-            error_print("expected status code '%d', got '%d'" % (expected_status_code, response.status_code))
+            error_print("expected status code '%d', got '%d'" % (expected_status_code,
+                                                                 response.status_code), url)
             return
         if response.text != expected_response:
             error_print("expected response '%s', got '%s'" % (expected_response, response.text))
@@ -31,24 +41,59 @@ def assert_get(url, expected_status_code, expected_response):
     color_print("SUCCESS: GET %s" % url, "\033[92m")
 
 
+def assert_redirect(url, expected_status_code, expected_response, expected_history_length=1):
+    try:
+        response = requests.get(url)
+        if len(response.history) != expected_history_length:
+            error_print("expected 1 redirect, got %d" % len(response.history), url)
+            return
+        response = response.history[0]
+        if response.status_code != expected_status_code:
+            error_print("expected status code '%d', got '%d'" % (expected_status_code,
+                                                                 response.status_code), url)
+            return
+        if response.text != expected_response:
+            error_print("expected response '%s', got '%s'" % (expected_response, response.text))
+    except Exception as e:
+        error_print("python exception: %s" % e, url)
+    else:
+        color_print("SUCCESS: GET redirect %s" % url, "\033[92m")
+
+
 def main():
-    # Launch the server in a subprocess
+    # Get the root path of the project
     root_path = __file__.split("/")
     root_path = "/".join(root_path[:-3])
     test_path = __file__.split("/")
     test_path = "/".join(test_path[:-1])
+
+    # Launch the server in a subprocess
     fork = subprocess.Popen([root_path + "/webserv", test_path + "/default.yaml"],
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=test_path)
+
     # Wait for the server to start
-    time.sleep(0.2)
-    # print(fork.stdout.read().decode("utf-8"))
+    try:
+        # server_log = fork.stderr.read().decode("utf-8")
+        exit_code = fork.wait(0.2)
+        error_print("server exited with code %d" % exit_code)
+        # color_print("== server log ==", BLUE)
+        # color_print(server_log, BLUE)
+        # color_print("====", BLUE)
+        return
+    except subprocess.TimeoutExpired:
+        color_print("server started", BLUE)
+
     # Test the server
     assert_get("http://localhost:8080/", 200, "<html>index</html>")
     assert_get("http://localhost:8080/index.html", 200, "<html>index</html>")
-    assert_get("http://localhost:8080/doesnotexist", 404,
-               "<!DOCTYPE html><html><head><title>Error Page</title></head><body>"
-               "<h1 style=\"text-align:center;\">404 Not Found</h1><p style=\"text-align:center;\">"
-               "42_webserv</p></body></html>")
+    assert_get("http://localhost:8080/index", 200, "<html>index</html>")
+    assert_get("http://localhost:8080/doesnotexist", 404, ERROR_404)
+    assert_get(HOST + "/photo/photo1", 200, "photo1")
+    assert_redirect(HOST + "/google", 302, "")
+    assert_redirect(HOST + "/redirect", 302, "", 2)
+    assert_redirect(HOST + "/recursive_redirect", 302, "", 3)
+    assert_redirect(HOST + "/mutual_redirect", 302, "", 2)
+    assert_get(HOST + "/cgi-bin/hello", 200, "Hello World!")
     fork.kill()
 
 
