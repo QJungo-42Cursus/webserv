7a1da508c22f3b947cd3abb83595a8ad65beb8b7
tplanes <tho.planes@gmail.com>
2023-05-26 15:18:42 +0200

    moved handle error from protected to public in RequestHandler.h

--- src/server/RequestHandler.h
+++ src/server/RequestHandler.h
@@ -10,13 +10,13 @@ public:
     virtual                 ~RequestHandler() {}
     virtual                 HttpResponse handle_request(const HttpRequest& request) = 0;
     RequestHandler(const Config *config);
+    HttpResponse            handle_error(int error_code, const std::string& error_phrase);
 protected:
     const Config*           config_;
 
     Route*                  find_route(const std::string& requested_path) const;
     bool                    is_method_allowed(const Route* route, const HttpRequest& request);
     std::string             create_error_html(int error_code, const std::string& error_phrase) const;
-    HttpResponse            handle_error(int error_code, const std::string& error_phrase);
     Option<HttpResponse>    parse(const HttpRequest& request);
     HttpResponse            handle_redirection(const Route* route);
 };
