cd52bce9275009f14378d667b63e49430075e3a0
Quentin Jungo <quentin.jungo@gmail.com>
2023-05-30 19:35:44 +0200

    e

--- src/utils/split.cpp
+++ src/utils/split.cpp
@@ -1,8 +1,6 @@
 #include <vector>
-#include <fstream>
 #include <sstream>
 #include <string>
-#include <iostream>
 
 std::vector<std::string> split(const std::string &str, char delim)
 {
@@ -11,5 +9,27 @@ std::vector<std::string> split(const std::string &str, char delim)
 	std::string item;
 	while (std::getline(ss, item, delim))
 		elems.push_back(item);
+	return elems;
+}
+
+std::vector<std::string> split(const std::string &str, const std::string &delim)
+{
+	std::vector<std::string> elems;
+	size_t pos = 0;
+	size_t len = str.length();
+	size_t delim_len = delim.length();
+	if (delim_len == 0)
+		return elems;
+	while (pos < len)
+	{
+		int find_pos = str.find(delim, pos);
+		if (find_pos < 0)
+		{
+			elems.push_back(str.substr(pos, len - pos));
+			break;
+		}
+		elems.push_back(str.substr(pos, find_pos - pos));
+		pos = find_pos + delim_len;
+	}
 	return elems;
 }
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -5,6 +5,11 @@
 #include <vector>
 
 #include "Option.h"
+
 std::vector<std::string> split(const std::string &str, char delim);
+
+std::vector<std::string> split(const std::string &str, const std::string &delim);
+
 std::string get_cwd();
+
 #endif // UTILS_H
