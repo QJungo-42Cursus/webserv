529f76a3d8ad69d9fef7592238056a00e9f2907c
qlentz <53540127+qlentz@users.noreply.github.com>
2023-05-30 15:07:55 +0200

    handler refactor

--- src/server/RequestHandler.cpp
+++ src/server/RequestHandler.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <sys/stat.h>
 #include <dirent.h>
+#include "../cgi_executor/CgiExecutor.h"
 
 RequestHandler::RequestHandler(const Config *config) : config_(config)
 {
@@ -261,75 +262,79 @@ HttpResponse GetRequestHandler::handle_request(const HttpRequest &request)
 		return handle_error(405, "Method Not Allowed");
 	}
 
-	std::string requested_path;
-  	std::string route_path;
-    std::string route_root;
-
 	if (route->redirection.isSome()) {
 		return handle_redirection(route);
 	}
 
-	if (route->root.isSome())
-	{
-		std::cout << "Route found: " << route->name << std::endl;
+	if (route->root.isNone()) {
+		return handle_error(404, "Not Found, no route");
+	}
 
-		requested_path = request.get_path();
-		route_path = route->name;
-		route_root = route->root.unwrap();
+	std::string requested_path;
+  	std::string route_path;
+    std::string route_root;
 
-		if (route_root[route_root.size() - 1] != '/')
+	requested_path = request.get_path();
+	route_path = route->name;
+	route_root = route->root.unwrap();
+
+	if (route_root[route_root.size() - 1] != '/')
 			route_root += '/';
 
+	if (requested_path[requested_path.size() - 1] != '/')
+		requested_path += '/';
+
+	size_t pos = requested_path.find(route_path);
+
+	if (pos != std::string::npos) {
+		requested_path.replace(pos, route_path.length(), route_root);
+	}
+
+	if (requested_path[requested_path.size() - 1] == '/')
+		requested_path.erase(requested_path.size() - 1);
+
+	bool is_file = is_path_file(requested_path);
+	bool is_directory = is_path_dir(requested_path);
+
+	if (!is_file && !is_directory)
+		return handle_error(404, "Not Found");
+
+
+	if (!is_file && is_directory)
+	{
 		if (requested_path[requested_path.size() - 1] != '/')
 			requested_path += '/';
-
-		size_t pos = requested_path.find(route_path);
-
-    	if (pos != std::string::npos) {
-        	requested_path.replace(pos, route_path.length(), route_root);
-    	}
-
-		if (requested_path[requested_path.size() - 1] == '/')
-			requested_path.erase(requested_path.size() - 1);
-
-		std::cout << "Requested_path : " << requested_path << std::endl;
-		bool is_file = is_path_file(requested_path);
-		bool is_directory = is_path_dir(requested_path);
-
-		std::cout << "Dir: " << is_directory << std::endl;
-		std::cout << "file: " << is_file << std::endl;
-		if (!is_file && !is_directory)
-			return handle_error(404, "Not Found");
-		if (!is_file && is_directory)
-		{
-			if (requested_path[requested_path.size() - 1] != '/')
-				requested_path += '/';
-			if (route->repertory_listing) {
-				response.set_body(dir_listing(requested_path, request.get_path()));
-				response.set_status(200, "OK");
-				response.add_header("Content-Type", "text/html");
-				return response;
-			}
-			else if (route->index.isSome()) {
-				requested_path += route->index.unwrap();
-				std::cout << "Index path: " << requested_path << std::endl;
-			}
-			else {
-				return handle_error(403, "Forbidden");
-			}
+		if (route->repertory_listing) {
+			response.set_body(dir_listing(requested_path, request.get_path()));
+			response.set_status(200, "OK");
+			response.add_header("Content-Type", "text/html");
+			return response;
 		}
-	}
-	else if (route->redirection.isSome()) {
-		//process http redirs
-		return handle_error(420, "Redir not implemented yet");
+		else if (route->index.isSome()) {
+			requested_path += route->index.unwrap();
+			std::cout << "Index path: " << requested_path << std::endl;
+		}
+		else {
+			return handle_error(403, "Forbidden");
+		}
 	}
 
 	std::ifstream file(requested_path.c_str(), std::ios::in | std::ios::binary);
 
+	if (route->cgi.isSome()) {
+		const CgiConfig &cgi = route->cgi.unwrap();
+		bool good_extension = requested_path.rfind(cgi.file_extension) == (requested_path.size() - cgi.file_extension.size());
+		if (good_extension) {
+        	std::string cgi_response = CgiExecutor::execute(request, *config_, cgi);
+			response.set_body(cgi_response);
+			response.set_status(200, "OK");
+			return response;
+		}
+	}
+
 	if (!file) {
 		return handle_error(404, "Not Found");
 	}
-
 	else {
 		std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 		response.set_body(content);
