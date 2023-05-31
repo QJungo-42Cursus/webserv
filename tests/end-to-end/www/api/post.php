53aefc9a2ad817ffbc998e7d657223eea5cb7fd1
Quentin Jungo <quentin.jungo@gmail.com>
2023-05-31 10:03:57 +0200

    Update post.php

--- tests/end-to-end/www/api/post.php
+++ tests/end-to-end/www/api/post.php
@@ -1,10 +1,12 @@
 <?php
 
 $line = trim(fgets(STDIN));
+$message = "<html><body><h1>hello</h1><p>you sent a GET request, good job</p><p>you sent me: " . $line . "</p> </body></html>";
+
 echo "Status: 200 OK\r\n";
 echo "Content-Type: text/plain\r\n";
-echo "Content-Length: " . strlen($line) . "\r\n";
+echo "Content-Length: " . strlen($message) . "\r\n";
 echo "\r\n";
-echo $line; 
+echo $message;
 
 ?>
